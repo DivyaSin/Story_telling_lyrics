bf999ac8aa9adc650c1d02f7bd068deb43585711
DivyaSin <divyasingh30@gmail.com>
2017-04-24 11:02:13 -0700

    Appended corpus

--- words/markov_lyrics.py
+++ words/markov_lyrics.py
@@ -3,17 +3,38 @@ import markovify
 import string
 import pprint
 from sylco import sylco
+import PyPDF2
+
+# pdfFileObj = open('rape_corpus.pdf', 'rb')
+# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+# page_nums = pdfReader.numPages
+
+# def get_pdf_content(pdf_path, page_nums):
+# 	content = ''
+# 	p = file(pdf_path, "rb")
+# 	pdf = PyPDF2.PdfFileReader(p)
+# 	for page_num in range(page_nums):
+# 		print page_num
+# 		content += pdf.getPage(page_num).extractText()
+# 	return content
+
+# pdf_content = get_pdf_content("rape_corpus.pdf", page_nums )
+# text_file = open('corpus/lyrics_batch.txt', 'w')  
+# print pdf_content
 
 def markov():
-	with open('/Users/divyasingh/Documents/MABLE/rape_corpus.txt') as f:
+	with open('/Users/divyasingh/Documents/MABLE/women_in_computing.txt') as f:
 		text = f.read()
-	reversed_lyrics = markovify.NewlineText(text)
+	reversed_lyrics = markovify.Text(text)
 	text_file = open('corpus/lyrics_batch.txt', 'w')  
-	for i in range(30):
-		print i
-		sentence = reversed_lyrics.make_short_sentence(60)
-		print sentence
+	for i in range(300):
+		# print i
+		sentence = reversed_lyrics.make_short_sentence(40)
+		# print sentence
 		if sentence:
-			text_file.write(sentence.translate(None, string.punctuation)+'.')
+			text_file.write(sentence.translate(None, string.punctuation) + '.')
+			# text_file.write(sentence)
     		text_file.write("\n")
 	text_file.close()
+
+markov()
