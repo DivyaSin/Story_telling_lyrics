5b55addf9f5b830690753795f73233adbc1965a6
DivyaSin <divyasingh30@gmail.com>
2017-10-30 00:05:32 -0700

    delete unnecessary files

--- words/FlaskApp/markov_lyrics.py
+++ words/FlaskApp/markov_lyrics.py
@@ -1,26 +1,5 @@
-import random
 import markovify
 import string
-import pprint
-from sylco import sylco
-import PyPDF2
-
-# pdfFileObj = open('rape_corpus.pdf', 'rb')
-# pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-# page_nums = pdfReader.numPages
-
-# def get_pdf_content(pdf_path, page_nums):
-# 	content = ''
-# 	p = file(pdf_path, "rb")
-# 	pdf = PyPDF2.PdfFileReader(p)
-# 	for page_num in range(page_nums):
-# 		print page_num
-# 		content += pdf.getPage(page_num).extractText()
-# 	return content
-
-# pdf_content = get_pdf_content("rape_corpus.pdf", page_nums )
-# text_file = open('corpus/lyrics_batch.txt', 'w')  
-# print pdf_content
 
 def markov():
 	with open('/Users/divyasingh/Documents/MABLE/Story_telling_lyrics/words/corpus/love_lyrics.txt') as f:
